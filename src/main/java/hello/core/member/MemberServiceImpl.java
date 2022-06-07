168fb3f38804cbc7fd33f1c4a51b913adb0f9120
YoungKyonYou <man.spride1324@gmail.com>
2022-06-07 17:49:32 +0900

    spring core study

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,8 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
+    //이런 형식은 MemberServiceImpl이 추상화에도 의존(memberRepository)하고 구체화에도(Memory MemberRepository)에도 의존한다.
+    //즉 둘다 의존한다 안좋다. 변경이 일어나게 되면 안 좋다. DIP를 위반하고 있다.
     private final MemberRepository memberRepository=new MemoryMemberRepository();
 
     @Override
